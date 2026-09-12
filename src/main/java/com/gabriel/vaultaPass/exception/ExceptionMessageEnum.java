081aad9a85ee42e5a95c8fe081e40cab691be74a
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2026-09-12 02:56:28 -0300

    feat: implements exception message enum

---
+++ src/main/java/com/gabriel/vaultaPass/exception/ExceptionMessageEnum.java
@@ -0,0 +1,26 @@
+package com.gabriel.vaultaPass.exception;
+
+public enum ExceptionMessageEnum {
+
+    USER_ID_IS_REQUIRED("The user id is required."),
+    PLATFORM_NAME_IS_REQUIRED("The platform name is required."),
+    LOGIN_IS_REQUIRED("The login is required."),
+    PASSWORD_IS_REQUIRED("The password is required."),
+    NAME_IS_REQUIRED("The name is required."),
+    LASTNAME_IS_REQUIRED("The lastname is required."),
+    USERNAME_IS_REQUIRED("The username is required."),
+    INVALID_PROFILE_PHOTO_URL("The profile photo url provided is invalid, please try another one."),
+    INVALID_EMAIL("The email address provided is invalid, please try another one."),
+    INVALID_LINK("The platform link provided is invalid, please try another one.");
+
+    private final String message;
+
+    ExceptionMessageEnum(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
