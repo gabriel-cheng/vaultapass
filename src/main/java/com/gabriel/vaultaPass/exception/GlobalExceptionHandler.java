c833f368a0047300d398d3e905a0f904eb432881
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2026-09-14 04:13:18 -0300

    chore: implements IllegalStateException handler

--- src/main/java/com/gabriel/vaultaPass/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gabriel/vaultaPass/exception/GlobalExceptionHandler.java
@@ -22,4 +22,11 @@ public class GlobalExceptionHandler {
                 .body(ex.getMessage());
     }
 
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+
 }
