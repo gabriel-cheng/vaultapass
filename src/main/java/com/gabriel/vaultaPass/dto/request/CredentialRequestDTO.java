cb65b600a3efb7afd9fdf7b6904f139a9c85d602
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2026-09-16 03:26:34 -0300

    chore: implements request dto validation

--- src/main/java/com/gabriel/vaultaPass/dto/request/CredentialRequestDTO.java
+++ src/main/java/com/gabriel/vaultaPass/dto/request/CredentialRequestDTO.java
@@ -1,11 +1,23 @@
 package com.gabriel.vaultaPass.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CredentialRequestDTO(
+
+    @NotBlank(message = "User id is required.")
     String userId,
+
+    @NotBlank(message = "Platform name is required.")
     String platformName,
+
+    @NotBlank(message = "Login is required.")
     String login,
+
+    @NotBlank(message = "Password is required.")
     String password,
+
     String email,
     String link,
     String description
+
 ) {}
